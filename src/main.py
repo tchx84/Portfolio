65e06e11aaf207391bc510c63a30aaa09da16657
Srevin Saju <srevinsaju@sugarlabs.org>
2020-12-27 13:36:27 +0300

    fix: require Handy v1 to prevent conflicts with system specific installation

--- src/main.py
+++ src/main.py
@@ -21,6 +21,7 @@ import gi
 gi.require_version("Gdk", "3.0")
 gi.require_version("Gtk", "3.0")
 gi.require_version("Gio", "2.0")
+gi.require_version("Handy", "1")
 
 from gi.repository import Gdk, Gtk, Gio
 
