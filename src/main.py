7da6e4661eed8d0eb508d533fd181e4fb638e37c
Srevin Saju <srevinsaju@sugarlabs.org>
2020-12-27 09:09:23 -0300

    qa: Add initial cleanup

--- src/main.py
+++ src/main.py
@@ -23,7 +23,7 @@ gi.require_version("Gtk", "3.0")
 gi.require_version("Gio", "2.0")
 gi.require_version("Handy", "1")
 
-from gi.repository import Gdk, Gtk, Gio, Handy
+from gi.repository import Gdk, Gtk, Gio
 
 from .window import PortfolioWindow
 
